1f7e11f3fea070aa875ec1bb85a1bbad02350294
Sohum Agrawal <sohum@sohums-air.3521.dhcp.asu.edu>
2023-10-28 23:41:49 -0700

    created logic for all team logos. kinda shitty since fanduel only has consistent urls for professional sports and I dont want to hard code anything. good enough for now

--- assetLookup/api.go
+++ assetLookup/api.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	_ "github.com/lib/pq"
@@ -39,11 +41,81 @@ func (s *AssetServer) StartAssetServerAPI(api fiber.Router) error {
 	assetsApi.Get("/:league/:teamName", func(c *fiber.Ctx) error {
 		leagueParsed := ConvertRawString(c.Params("league"))
 		teamName := ConvertRawString(c.Params("teamName"))
-		url := fmt.Sprintf("https://assets.sportsbook.fanduel.com/images/team/%s/%s.png", leagueParsed, teamName)
-		c.Write([]byte(url))
+
+		if leagueParsed == "nfl" || leagueParsed == "nba" || leagueParsed == "nhl" {
+			url := fmt.Sprintf("https://assets.sportsbook.fanduel.com/images/team/%s/%s.png", leagueParsed, teamName)
+			c.Write([]byte(url))
+		} else if leagueParsed == "ncaaf" {
+			url := s.GetCollegeFootballLogo()
+			c.Write([]byte(url))
+		} else if leagueParsed == "ncaab" {
+			url := s.GetCollegeBasketballLogo()
+			c.Write([]byte(url))
+		} else {
+			url := s.GetSoccerLogo()
+			c.Write([]byte(url))
+		}
+
 		return nil
 	})
 	return nil
+}
+
+func (s *AssetServer) GetSoccerLogo() string {
+	urls := []string{
+		"https://assets.sportsbook.fanduel.com/images/team/soccer/epl/west_ham.png",
+		"https://assets.sportsbook.fanduel.com/images/team/soccer/epl/everton.png",
+		"https://assets.sportsbook.fanduel.com/images/team/soccer/epl/brighton.png",
+		"https://assets.sportsbook.fanduel.com/images/team/soccer/epl/fulham.png",
+		"https://assets.sportsbook.fanduel.com/images/team/soccer/epl/aston_villa.png",
+		"https://assets.sportsbook.fanduel.com/images/team/soccer/epl/luton.png",
+		"https://assets.sportsbook.fanduel.com/images/team/soccer/epl/liverpool.png",
+		"https://assets.sportsbook.fanduel.com/images/team/soccer/epl/nottingham_forest.png",
+		"https://assets.sportsbook.fanduel.com/images/team/soccer/epl/man_utd.png",
+		"https://assets.sportsbook.fanduel.com/images/team/soccer/epl/man_city.png",
+	}
+
+	rand.Seed(time.Now().Unix())
+	n := rand.Int() % len(urls)
+	return urls[n]
+}
+
+func (s *AssetServer) GetCollegeFootballLogo() string {
+	urls := []string{
+		"https://assets.sportsbook.fanduel.com/images/team/ncaaf/san_jose_state.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaaf/hawaii.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaaf/northern_illinois_huskies.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaaf/central_michigan.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaaf/buffalo.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaaf/toledo_rockets.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaaf/kent_state.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaaf/akron_zips.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaaf/penn_state_lions.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaaf/maryland.png",
+	}
+
+	rand.Seed(time.Now().Unix())
+	n := rand.Int() % len(urls)
+	return urls[n]
+}
+
+func (s *AssetServer) GetCollegeBasketballLogo() string {
+	urls := []string{
+		"https://assets.sportsbook.fanduel.com/images/team/ncaab/lsu.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaab/connecticut.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaab/iowa.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaab/virginia_tech.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaab/utah.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaab/indiana.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaab/south_carolina.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaab/stanford.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaab/north_carolina.png",
+		"https://assets.sportsbook.fanduel.com/images/team/ncaab/duke.png",
+	}
+
+	rand.Seed(time.Now().Unix())
+	n := rand.Int() % len(urls)
+	return urls[n]
 }
 
 func (s *AssetServer) Start(api fiber.Router) error {
@@ -60,7 +132,7 @@ func ConvertRawString(raw string) string {
 	return new
 }
 
-// func (s *AssetServer) ConvertToUrl(league, team string) string {
+// func (s *AssetServer) ConvertToleague, team string) string {
 // 	convertedLeague := ConvertRawString(league)
 // 	convertedTeam := ConvertRawString(team)
 // 	if convertedLeague == "nba" || convertedTeam == "nba" {
